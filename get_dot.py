8e45da17ac4a205839bfb71b2d452de1f7ff57ef
Olivia Hsu <owhsu@stanford.edu>
2024-08-14 13:17:39 -0700

    Update get_dot.py

--- get_dot.py
+++ get_dot.py
@@ -1,4 +1,5 @@
 import tortilla_pb2
+import comal_pb2
 from google.protobuf import text_format
 import pydot
 import argparse
@@ -8,17 +9,18 @@ def process():
                     prog='Tortilla Visualizer',
                     description='Converts samml protobuf output to dot png')
     parser.add_argument('-f', '--file', required=True)
+    parser.add_argument('-o', '--outfile', default='graph.png')
     args = parser.parse_args()
     with open(args.file, "r") as f:
         proto_text = f.read()
-    program = tortilla_pb2.ProgramGraph()
+    program = comal_pb2.ComalGraph()
     text_format.Parse(proto_text, program)
 
     result = "digraph SAM {\n"
-    result += process_proto(program)
+    result += process_proto(program.graph)
     result += "}"
     (graph,) = pydot.graph_from_dot_data(result)
-    out_file = "graph.png"
+    out_file = args.outfile
     graph.write_png(out_file)
     print("Generated graph in: " + out_file)
 
